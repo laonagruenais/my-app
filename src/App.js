e91c3da6f3f586cffef9989337886634f236611c
laonagruenais <emmie.laona@gmail.com>
2023-05-04 13:49:35 +0200

    app-test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [races, setRaces] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/races")
+      .then((response) => response.json())
+      .then((data) => setRaces(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const titleStyle = {
+    textAlign: "center",
+    fontSize: "2em",
+    fontWeight: "bold",
+    marginBottom: "1em",
+  };
+
+  const raceStyle = {
+    display: "inline-block",
+    padding: "0.5em",
+    margin: "0.5em",
+    backgroundColor: "#eee",
+    borderRadius: "0.25em",
+    boxShadow: "0 0 0.25em rgba(0, 0, 0, 0.1)",
+    textTransform: "capitalize",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={titleStyle}>Liste des races de chien</h1>
+      <ul>
+        {races.map((race) => (
+          <li key={race} style={raceStyle}>
+            {race}{" "}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
